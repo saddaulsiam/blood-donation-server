17c4dedc8277904a33185bd512f255b77c020b47
Saddaul Siam <saddaulsiam@gmail.com>
2025-07-16 23:01:56 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,23 @@
 import cookieParser from "cookie-parser";
 import cors, { CorsOptions } from "cors";
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import APINotFound from "./app/middlewares/APINotFound";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 import router from "./app/routes";
 
 const app = express();
 
-const corsOptions = {
-  origin: "https://blood-donation24.vercel.app",
+// const allowedOrigins = ["https://blood-donation24.vercel.app", "http://localhost:3000"];
+/*  (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  } ,*/
+
+const corsOptions: CorsOptions = {
+  origin: ["https://blood-donation24.netlify.app"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
